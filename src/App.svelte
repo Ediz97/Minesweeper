ab77e8fbb1be4c83caea9f638ce152b8d8f3e17d
Ediz Turan <edizturan97@gmail.com>
2023-01-22 00:47:50 +0100

    1.0

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+    import UserInterface from './lib/UserInterface.svelte';
+    import DifficultySelection from "./lib/DifficultySelection.svelte";
+
+</script>
+
+<main>
+    <DifficultySelection/>
+    <UserInterface/>
+    <img src="bobomb.png" alt="A bomb.">
+</main>
+
+<style>
+    img {
+        position: fixed;
+        height: 339px;
+        width: 292px;
+        bottom: 30px;
+        right: 15px;
+        pointer-events: none;
+        user-select: none;
+    }
+    
+    main {
+        caret-color: transparent;
+    }
+</style>
